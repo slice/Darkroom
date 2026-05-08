e21339b5108a9b7052d8d9121fc6770e57356a62
Skip R <tinyslices@gmail.com>
2026-05-07 17:22:48 -0700

    solarium: clean up

--- Sources/DRDisableSolarium.mm
+++ Sources/DRDisableSolarium.mm
@@ -1,16 +1,17 @@
 #import "DRDefaults.h"
 #import "DRLog.h"
 #import <Foundation/Foundation.h>
+#import <array>
 #import <dlfcn.h>
-#import <initializer_list>
 #import <objc/runtime.h>
 
+// we are patching SPI
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
 
 namespace {
 
-auto _DRSolariumEnabled(id /*self*/, SEL /*_cmd*/) -> BOOL
+BOOL _DRSolariumEnabled(id /*self*/, SEL /*_cmd*/)
 {
 	return [DRDefaults isSolariumEnabled];
 }
@@ -19,15 +20,16 @@ __attribute__((constructor(101))) void DRDisableSolarium()
 {
 	DRLog(@"Darkroom: has entered the building :3");
 
-	Class _NSSolarium = NSClassFromString(@"_NSSolarium");
+	Class _NSSolarium { NSClassFromString(@"_NSSolarium") };
 	if (_NSSolarium == nullptr) {
 		DRLog(@"Darkroom: _NSSolarium is missing, bailing");
 		return;
 	}
 
-	Class metaclass = object_getClass(_NSSolarium);
-	for (auto* selector : { @selector(isEnabled), @selector(isEnabledIgnoringCompatibility) }) {
-		Method method = class_getInstanceMethod(metaclass, selector);
+	Class metaclass { object_getClass(_NSSolarium) };
+	auto selectors = std::array { @selector(isEnabled), @selector(isEnabledIgnoringCompatibility) };
+	for (SEL selector : selectors) {
+		Method method { class_getInstanceMethod(metaclass, selector) };
 		if (method != nullptr) {
 			method_setImplementation(method, reinterpret_cast<IMP>(_DRSolariumEnabled));
 		} else {
